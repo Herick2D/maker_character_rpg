75c0e9de929e4503644ae18ed989e84b553e9606
walefy <walefyd1@gmail.com>
2023-11-17 17:36:50 -0300

    refactor: remove unecessary type

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,10 +1,9 @@
 import { CreateUserType } from '../types/createUserBody';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import * as userService from '../service/user.service';
-import { CustomRequest } from '../types/CustomRequest';
 import { mapHttpStatusCode } from '../utils/mapHttpStatusCode';
 
-export const createUser = async (req: CustomRequest<CreateUserType>, res: Response) => {
+export const createUser = async (req: Request<object, object, CreateUserType>, res: Response) => {
 	const { name, email, password } = req.body;
 	const serviceResponse = await userService.createUser(name, email, password);
 
